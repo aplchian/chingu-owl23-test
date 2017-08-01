c4e596ff388613d6a4eecc8113bc59d7ae5fc60b
codekevcode <newtonkevin2010@gmail.com>
2017-08-01 11:06:20 -0400

    React/Bootstrap Splash Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Bootstrap.css'
 
 class App extends Component {
   render() {
     return (
+      <div className ="jumbotron jumbotron-fluid align-baseline" style={{backgroundImage:'url(public/img/bgimg.jpg)'}}>
+          <div className ="container">
+            <h1> Chingu Owls team 23 React Splash Page </h1>
+            <p> with Bootstrap and SASS </p>
+            <p><a className="btn btn-primary" href="http://www.google.com" target="_blank"> Discovery things</a></p>
+          </div>
+        </div>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
