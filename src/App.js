939e5620e49c27b0d3b47a428cd8f94ce2de6813
codekevcode <newtonkevin2010@gmail.com>
2017-08-01 11:59:39 -0400

    adding a background image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className ="jumbotron jumbotron-fluid align-baseline" style={{backgroundImage:'url(public/bgimg.jpg)'}}>
+      <div className ="jumbotron jumbotron-fluid align-baseline" style={{backgroundImage:'url(../public/bgimg.jpg)'}}>
        <div className ="container">
          <h1> Chingu Owls team 23 React Splash Page </h1>
          <p> with Bootstrap and SASS </p>
