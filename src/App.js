a01c1e9decaafbd7e002ca81cecdd90d3d2973e5
codekevcode <newtonkevin2010@gmail.com>
2017-08-01 21:39:32 -0400

    Panel

--- src/App.js
+++ src/App.js
@@ -6,28 +6,32 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div>
       <div className ="jumbotron jumbotron-fluid align-baseline" style={{backgroundImage:'url(bgimg.jpg)'}}>
        <div className ="container">
          <h1> Chingu Owls team 23 React Splash Page </h1>
          <p> with Bootstrap and SASS </p>
          <p><a className="btn btn-primary" href="http://www.google.com" target="_blank"> Discovery things</a></p>
-       </div>
-       div className="panel panel-default">
-   <div className="panel-heading">Panel heading without title</div>
-   <div className="panel-body">
-     Panel content
-   </div>
- </div>
 
- <div className="panel panel-default">
-   <div className="panel-heading">
-     <h3 className="panel-title">Panel title</h3>
-   </div>
-   <div className="panel-body">
-     Panel content
-   </div>
- </div>
+       </div>
+
      </div>
+     div className="panel panel-default">
+ <div className="panel-heading">Panel heading without title</div>
+ <div className="panel-body">
+   Panel content
+ </div>
+</div>
+
+<div className="panel panel-default">
+ <div className="panel-heading">
+   <h3 className="panel-title">Panel title</h3>
+ </div>
+ <div className="panel-body">
+   Panel content
+ </div>
+</div>
+</div>
     );
   }
 }
