584884f15c62d8dd8b05e4d7a32bc4e6513ad14b
codekevcode <newtonkevin2010@gmail.com>
2017-08-01 21:03:11 -0400

    Panel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AttentionGrabber } from './fold';
+
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { AttentionGrabber } from './fold';
 
+import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AttentionGrabber />, document.getElementById('top'));
 registerServiceWorker();
