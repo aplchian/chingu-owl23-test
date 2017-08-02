2b2c75d92dac209e5caf3617a006a55c75da457a
codekevcode <newtonkevin2010@gmail.com>
2017-08-01 21:28:22 -0400

    Panel

--- src/fold.js
+++ src/fold.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class AttentionGrabber extends React.Component {
+class AttentionGrabber extends Component {
 	render() {
 		return (
       <div>
